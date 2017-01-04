bf555b3456095fce145827353212d8ab1d579fca
SmithTM90 <smithtm90@gmail.com>
2017-01-04 15:17:50 -0800

    adding controllers

---
+++ js/controllers.js
@@ -0,0 +1,21 @@
+jsApp.controller('HomeCtrl', function($scope){
+
+	$(document).ready(function(){
+	});
+});
+
+jsApp.controller('PortfolioCtrl', function($scope, $route){
+
+	$(document).ready(function(){
+	});
+});
+
+jsApp.controller('AboutCtrl', function(){
+	$(document).ready(function(){
+	});
+});
+
+jsApp.controller('ContactCtrl', function(){
+	$(document).ready(function(){
+	});
+});
