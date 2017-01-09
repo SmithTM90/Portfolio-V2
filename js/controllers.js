2ed4f518d90a1fe7f35e41400d6e9c4e7917aa61
SmithTM90 <smithtm90@gmail.com>
2017-01-09 15:11:09 -0800

    Updating styling on home page

--- js/controllers.js
+++ js/controllers.js
@@ -1,21 +1,19 @@
-jsApp.controller('HomeCtrl', function($scope){
-
+portApp.controller('HomeCtrl', function($scope){
 	$(document).ready(function(){
 	});
 });
 
-jsApp.controller('PortfolioCtrl', function($scope, $route){
-
+portApp.controller('PortfolioCtrl', function($scope, $route){
 	$(document).ready(function(){
 	});
 });
 
-jsApp.controller('AboutCtrl', function(){
+portApp.controller('AboutCtrl', function(){
 	$(document).ready(function(){
 	});
 });
 
-jsApp.controller('ContactCtrl', function(){
+portApp.controller('ContactCtrl', function(){
 	$(document).ready(function(){
 	});
 });
