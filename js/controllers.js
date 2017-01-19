cb39810287ec0aad6f34c736120261aecfc1ec1e
SmithTM90 <smithtm90@gmail.com>
2017-01-19 14:22:10 -0800

    second iteration of about me transition

--- js/controllers.js
+++ js/controllers.js
@@ -5,11 +5,11 @@ portApp.controller('HomeCtrl', function($scope){
 		var aboutSecond = $('#aboutSecond').text();
 
 		$('.peerQuote').mouseenter( function() {
-			$(this).html('My Skills - Languages: Javascript, HTML5, CSS3, Ruby, SQL' <br> 'Frameworks: Angular, Node, Express, Rails' <br> 'Libraries: Bootstrap, jQuery, D3' <br> 'Databases: PostgreSQL, MongoDB, Active Record' <br> 'Design: Animations, Web Fonts, Mobile First Design, Image Manipulation' <br> 'Other: RESTful Routing, Authentication, Authorization, JSON, AJAX, JWT, Sessions, TDD, Agile');
+			$(this).html('My Skills - Languages: Javascript, HTML5, CSS3, Ruby, SQL <br> Frameworks: Angular, Node, Express, Rails <br> Libraries: Bootstrap, jQuery, D3 <br> Databases: PostgreSQL, MongoDB, Active Record <br> Design: Animations, Web Fonts, Mobile First Design, Image Manipulation <br> Other: RESTful Routing, Authentication, Authorization, JSON, AJAX, JWT, Sessions, TDD, Agile');
 		});
 
 		$('.peerQuote').mouseleave(function() {
-			$this.html(aboutFirst);
+			$(this).html(aboutFirst);
 		})
 
 	});
