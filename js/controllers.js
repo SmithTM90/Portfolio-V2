44784f10b573314b1c582c8c47f4b434229ce32b
SmithTM90 <smithtm90@gmail.com>
2017-01-19 14:20:29 -0800

    first attempt of about me transition to skills

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,17 @@
 portApp.controller('HomeCtrl', function($scope){
 	$(document).ready(function(){
+
+		var aboutFirst = $('#aboutFirst').text();
+		var aboutSecond = $('#aboutSecond').text();
+
+		$('.peerQuote').mouseenter( function() {
+			$(this).html('My Skills - Languages: Javascript, HTML5, CSS3, Ruby, SQL' <br> 'Frameworks: Angular, Node, Express, Rails' <br> 'Libraries: Bootstrap, jQuery, D3' <br> 'Databases: PostgreSQL, MongoDB, Active Record' <br> 'Design: Animations, Web Fonts, Mobile First Design, Image Manipulation' <br> 'Other: RESTful Routing, Authentication, Authorization, JSON, AJAX, JWT, Sessions, TDD, Agile');
+		});
+
+		$('.peerQuote').mouseleave(function() {
+			$this.html(aboutFirst);
+		})
+
 	});
 });
 
