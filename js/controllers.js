9a85d7b136fec5f90573b1d6de3038e701875073
SmithTM90 <smithtm90@gmail.com>
2017-01-23 13:39:09 -0800

    I think this update was for the main page about me sections

--- js/controllers.js
+++ js/controllers.js
@@ -1,16 +1,24 @@
 portApp.controller('HomeCtrl', function($scope){
 	$(document).ready(function(){
 
-		var aboutFirst = $('#aboutFirst').text();
-		var aboutSecond = $('#aboutSecond').text();
+		// var aboutFirst = $('#aboutFirst').text();
+		// var aboutSecond = $('#aboutSecond').text();
 
-		$('.peerQuote').mouseenter( function() {
-			$(this).html('My Skills - Languages: Javascript, HTML5, CSS3, Ruby, SQL <br> Frameworks: Angular, Node, Express, Rails <br> Libraries: Bootstrap, jQuery, D3 <br> Databases: PostgreSQL, MongoDB, Active Record <br> Design: Animations, Web Fonts, Mobile First Design, Image Manipulation <br> Other: RESTful Routing, Authentication, Authorization, JSON, AJAX, JWT, Sessions, TDD, Agile');
-		});
+		// $('#blurb1').mouseenter( function() {
+		// 	$(this).html('My Skills - Languages: Javascript, HTML5, CSS3, Ruby, SQL <br> Frameworks: Angular, Node, Express, Rails <br> Libraries: Bootstrap, jQuery, D3 <br> Databases: PostgreSQL, MongoDB, Active Record <br> Design: Animations, Web Fonts, Mobile First Design, Image Manipulation <br> Other: RESTful Routing, Authentication, Authorization, JSON, AJAX, JWT, Sessions, TDD, Agile');
+		// });
 
-		$('.peerQuote').mouseleave(function() {
-			$(this).html(aboutFirst);
-		})
+		// $('#blurb1').mouseleave(function() {
+		// 	$(this).html(aboutFirst);
+		// });
+
+		// $('#blurb2').mouseenter( function() {
+		// 	$(this).html('My Skills - Languages: Javascript, HTML5, CSS3, Ruby, SQL <br> Frameworks: Angular, Node, Express, Rails <br> Libraries: Bootstrap, jQuery, D3 <br> Databases: PostgreSQL, MongoDB, Active Record <br> Design: Animations, Web Fonts, Mobile First Design, Image Manipulation <br> Other: RESTful Routing, Authentication, Authorization, JSON, AJAX, JWT, Sessions, TDD, Agile');
+		// });
+
+		// $('#blurb2').mouseleave(function() {
+		// 	$(this).html(aboutSecond);
+		// });
 
 	});
 });
